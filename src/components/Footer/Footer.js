656c0924085c3af6a767038972e0f1e8523d8f45
seymafirat <seyma.firat@skinvision.com>
2022-05-31 14:34:38 +0200

    footer lists

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,17 +4,45 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-div">
-        <div className="footer-div">
+        <div className="footer-div-item">
           <h2>Studio</h2>
+          <ul className="footer-div-list">
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+            
+          </ul>
         </div>
-        <div className="footer-div">
+        <div className="footer-div-item">
           <h2>Games</h2>
+          <ul className="footer-div-list">
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+            
+          </ul>
         </div>
-        <div className="footer-div">
+        <div className="footer-div-item">
           <h2>Support</h2>
+          <ul className="footer-div-list">
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+            
+          </ul>
         </div>
-        <div className="footer-div">
+        <div className="footer-div-item">
           <h2>Logo</h2>
+          <ul className="footer-div-list">
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+           <li className="footer-list-item"> <a className="footer-list-link" href="/about">Lorem</a></li>
+            
+          </ul>
         </div>
       </div>
       <p className="footer-text">Copyright © Myth Game Studio</p>
