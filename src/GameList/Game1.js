448ed269839f488d986f551a93f90ee7a2be8891
seymafirat <seyma.firat@skinvision.com>
2022-06-08 15:52:09 +0200

    carousel added

--- src/GameList/Game1.js
+++ src/GameList/Game1.js
@@ -11,9 +11,9 @@ function Game1() {
       height={300}
       width={500}
       />
-      <h2>Assembly Point</h2> </div>
+      <h2 className="game-title">Assembly Point</h2> </div>
 
-        <div className="game-text">      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce erat purus, suscipit quis euismod ac, viverra quis sapien. Aenean ac arcu tincidunt, vestibulum nulla a, mattis nisi. Mauris rutrum at libero ut bibendum. Suspendisse ultricies imperdiet metus nec iaculis. Mauris et vehicula tortor. Praesent non purus sit amet velit cursus tristique quis sit amet dolor. Etiam commodo semper est a viverra. Integer vel diam bibendum, viverra diam ac, luctus magna. Suspendisse tellus nulla, feugiat non orci id, sodales molestie eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer eget quam et diam convallis tristique sed sagittis lorem. Vivamus finibus suscipit pharetra.</p>
+        <div className="game-text">      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce erat purus, suscipit quis euismod ac, viverra quis sapien. Aenean ac arcu tincidunt, vestibulum nulla a, mattis nisi. Mauris rutrum at libero ut bibendum. Suspendisse ultricies imperdiet metus nec iaculis. Mauris et vehicula tortor. Praesent non purus sit amet velit cursus tristique quis sit amet dolor. Etiam commodo semper est a viverra. Integer vel diam bibendum, viverra diam ac, luctus magna. Suspendisse tellus nulla, feugiat non orci id, sodales molestie eros.</p>
 </div></div>
       
     </div>
